db984ed85f2451c960566ecde128f37994a1af9e
Rikellme Keven Andrade de Lima <rikellmekevin@gmail.com>
2022-02-24 15:01:13 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const email = document.getElementById('email').value
+
+document.querySelector("button").addEventListener('click', () => {
+    alert("Email " + document.getElementById('email').value + " enviado com sucesso !!")
+})
